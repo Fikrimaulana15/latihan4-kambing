dd570695ca62092e22eac90f0ed261c74adb8844
fikrimaualana <55828126+Fikrimaulana15@users.noreply.github.com>
2019-09-26 15:21:05 +0700

    selesai

---
+++ src/pbo2/pkg10118074/latihankambing/Pbo210118074Latihankambing.java
@@ -0,0 +1,32 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pbo2.pkg10118074.latihankambing;
+
+/**
+ *
+ * @author 
+ * NAMA : FikriMaulana
+ * KELAS : IF-2
+ * NIM : 10118074
+ */
+public class Pbo210118074Latihankambing {
+
+        public void tambahkambing() {
+            //Deklarasi variabel lokal
+            int jumlahKambing = 0;
+            
+            jumlahKambing = jumlahKambing + 5;
+            System.out.println("Jumlah Kambing setelah ditambah: " + jumlahKambing);
+        }
+        
+        public static void main(String[] args) {
+            Pbo210118074Latihankambing kambingJantan = new Pbo210118074Latihankambing();
+            kambingJantan.tambahkambing();
+        
+        }
+        
+    
+}
